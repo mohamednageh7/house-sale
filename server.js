fb64fe0844fd587f8c46970ba28f460527b9df4a
mohamed nageh <albanza38@gamil.com>
2020-08-17 22:01:40 +0300

    edit experess static

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./client/build'));
+app.use(express.static('./build'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
